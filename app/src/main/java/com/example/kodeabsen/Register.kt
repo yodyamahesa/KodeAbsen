86d0233054e0c952f1d27676e747f3502348da7a
Yodya Mahesa <yodyamahesa123@gmail.com>
2023-05-09 22:16:57 +0800

    Intent Register udah Aman

--- app/src/main/java/com/example/kodeabsen/Register.kt
+++ app/src/main/java/com/example/kodeabsen/Register.kt
@@ -1,11 +1,36 @@
 package com.example.kodeabsen
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class Register: AppCompatActivity() {
+
+    lateinit var usernameEditText: EditText
+    lateinit var emailEditText: EditText
+    lateinit var passwordEditText: EditText
+    lateinit var confirmpasswordEditText: EditText
+    lateinit var daftarButton: Button
+
+    fun initComponnents(){
+        usernameEditText = findViewById(R.id.username_Register_EditText)
+        emailEditText = findViewById(R.id.email_Register_EditText)
+        passwordEditText = findViewById(R.id.password_Register_EditText)
+        confirmpasswordEditText = findViewById(R.id.confirmpassword_Register_EditText)
+        daftarButton = findViewById(R.id.daftar_Register_Button)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+
+        initComponnents()
+
+        daftarButton.setOnClickListener{
+            val keRegisterDone = Intent(this, RegisterDone::class.java)
+            finish()
+            startActivity(keRegisterDone)
+        }
     }
 }
--- app/src/main/java/com/example/kodeabsen/RegisterDone.kt
+++ app/src/main/java/com/example/kodeabsen/RegisterDone.kt
@@ -1,11 +1,28 @@
 package com.example.kodeabsen
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterDone: AppCompatActivity() {
+
+    lateinit var okeButton: Button
+
+    fun initComponents(){
+        okeButton = findViewById(R.id.oke_RegisterDone_Button)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_done)
+
+        initComponents()
+
+        okeButton.setOnClickListener{
+            val keLogin = Intent(this, Login::class.java)
+            finish()
+            startActivity(keLogin)
+        }
     }
 }
